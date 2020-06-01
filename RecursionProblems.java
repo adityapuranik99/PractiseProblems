5169ce81415b78bf02fd3ecacb852db195afca6f
adityapuranik99 <adityapuranik99@gmail.com>
2020-06-02 00:22:19 +0530

    Update RecursionProblems.java

--- RecursionProblems.java
+++ RecursionProblems.java
@@ -1,12 +1,10 @@
 class Recursion {
     public static void main(String[] args) {
-        int k = 0;
-        String str = "abcba";
-        int n = str.length() - 1;
-        if (isPal(str, k, n))
-            System.out.print("True");
-        else
-            System.out.print("False");
+        int n = 5;
+        int a = 2;
+        int b = 5;
+        int c = 1;
+        System.out.print(maxCuts(n, a, b, c));
     }
 
     // Basic function to print n numbers
@@ -43,5 +41,60 @@ class Recursion {
             }
         }
         return false;
+    }
+
+    // Josepher's Problem, given n people k th person is killed in each iteration
+
+    static int jose(int n, int k) {
+        if (n == 1)
+            return n;
+        return (((jose(n - 1, k) + k - 1) % n + 1));
+    }
+
+    // Function to find sum of digits
+    static int sumOfDigits(int n) {
+        if (n == 0)
+            return 0;
+
+        return n % 10 + sumOfDigits(n / 10);
+    }
+
+    // Given a rope of length n, you need to find the max. no of pieces
+    static int maxCuts(int n, int a, int b, int c) {
+        if (n == 0)
+            return 0;
+        if (n < 0)
+            return -1;
+
+        int res = max(maxCuts(n - a, a, b, c), maxCuts(n - b, a, b, c), maxCuts(n - c, a, b, c));
+
+        if (res == -1)
+            return -1;
+        else
+            return res + 1;
+    }
+
+    // Maximum of three nums, Temporary function as a subset of another question
+
+    static int max(int a, int b, int c) {
+        int max = a;
+        if (b > a)
+            max = b;
+        if (c > b)
+            max = c;
+        if (c > a)
+            max = c;
+        return max;
+    }
+
+    // Given a string print all the subsets
+    static void subSet(String str, String curr, int index) {
+        if (index < str.length()) {
+            subSet(str, curr, index + 1);
+            subSet(str, curr += (str.charAt(index)), index + 1);
+        } else {
+            System.out.println(curr);
+            return;
+        }
     }
 }
