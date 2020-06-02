8d2604e67d7cbae614e3f65a74c729fa75957567
adityapuranik99 <adityapuranik99@gmail.com>
2020-06-02 11:23:25 +0530

    Create ReverseArray.java

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,36 @@
+package Arrays;
+
+class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 2, 5, 6 };
+        rotateArr(arr, 2);
+        printArr(arr);
+    }
+
+    // To reverse an array
+    static void ReverseArr(int[] arr, int l, int h) {
+        while (l < h) {
+            int temp = arr[h];
+            arr[h] = arr[l];
+            arr[l] = temp;
+            l++;
+            h--;
+        }
+    }
+
+    // To rotate an array from a given point d
+    static void rotateArr(int[] arr, int d) {
+        ReverseArr(arr, 0, d - 1);
+        System.out.println(" ");
+        ReverseArr(arr, d, arr.length - 1);
+        System.out.println(" ");
+        ReverseArr(arr, 0, arr.length - 1);
+    }
+
+    static void printArr(int[] arr) {
+        for (int i = 0; i < (arr.length); i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
